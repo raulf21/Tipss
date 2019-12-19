2f65730757b0ce469c6027294377b6eba9b58565
Raul Flores <raulflores650@gmail.com>
2019-12-18 18:31:08 -0800

    Initial Commit

---
+++ Tipss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipss
+//
+//  Created by Raul Flores on 12/18/19.
+//  Copyright © 2019 Raul Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
